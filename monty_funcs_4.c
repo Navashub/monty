674f3464fe720ef2c128c40caf59c1bcf0f4310c
Vivi-anne <viviannekwamboka98@gmail.com>
2023-03-23 19:00:44 -0700

    final updates

--- monty_funcs_4.c
+++ monty_funcs_4.c
@@ -6,7 +6,8 @@ void monty_stack(stack_t **stack, unsigned int line_number);
 void monty_queue(stack_t **stack, unsigned int line_number);
 
 /**
- * monty_rotl - Rotates the top value of a stack_t linked list to the bottom.
+ * monty_rotl - Rotates the top value of a stack_t linked 
+ * list to the bottom.
  * @stack: A pointer to the top mode node of a stack_t linked list.
  * @line_number: The current working line number of a Monty bytecodes file.
  */
